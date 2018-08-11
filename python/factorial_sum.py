0c84528ef1b560ce00ebb9d9a57375636fc6811f
Broono Lu <broono@qq.com>
2018-08-11 17:09:40 +0800

    Add factorial_sum.py

---
+++ python/factorial_sum.py
@@ -0,0 +1,17 @@
+"""A simple wrapper to calculate factorial sum from of a range."""
+
+
+def factorial(n: 'int >= 0', history={}) -> int:
+    if n < 0:
+        raise ValueError(f"Non-negative expected, got {n}")
+    if n == 0 or n == 1:
+        fact = 1
+    else:
+        fact = n * factorial(n - 1)
+    if n not in history:
+        history[n] = fact
+    return fact
+
+
+def sum_of_factorials(n: 'int >= 0', start: 'int >= 0'=0) -> int:
+    return sum(map(factorial, range(min(n, start), max(n, start) + 1)))
