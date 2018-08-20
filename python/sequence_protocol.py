332c6bd8f2b4337fbb1f2d31183e5607bc1e008b
Broono Lu <broono@qq.com>
2018-08-21 00:34:00 +0800

    Add doctest and repr and str magic method

--- python/sequence_protocol.py
+++ python/sequence_protocol.py
@@ -8,6 +8,15 @@ class Points:
     """This is a simple example of sequence protocol, which
     entails just the __len__ and __getitem__ magic methods.
     __getitem__ is all that's needed to support iteration.
+
+    >>> points = Points([Point(x) for x in range(10)]); print(len(points))
+    10
+    >>> points[0]
+    Point(0)
+    >>> points[5]
+    Point(5)
+    >>> points[2:5]
+    Points([Point(2), Point(3), Point(4)])
     """
 
     def __init__(self, points: Sequence[Point]):
@@ -19,3 +28,12 @@ class Points:
         if isinstance(index, Integral):
             return self._points[index]
         raise TypeError(f"{self.__class__.__name__} indices must be integers")
+
+    def __len__(self):
+        return len(self._points)
+
+    def __repr__(self):
+        return f'Points({self._points})'
+
+    def __str__(self):
+        return ', '.join([str(point) for point in self._points]).join('[]')
